ba63f6b40f030670a635dd2aad88dd6c01b10163
unknown <koraviita@gmail.com>
2022-04-18 02:01:36 +0300

    Added functionality.
    Added weather data to comments.

--- WeatherApp/components/weatherData.js
+++ WeatherApp/components/weatherData.js
@@ -3,11 +3,20 @@ import {View, Text, StyleSheet, TouchableOpacity, Modal, Button, TextInput} from
 import TopNav from './topNav';
 import BotNav from './botNav';
 import { getLocation } from '../API/API';
+import {db} from '../firebase';
+import { collection, getDocs, getDoc, query, where, doc, deleteDoc,addDoc} from "firebase/firestore";
 
 
 const WeatherLines = props =>{
+  const [savedComment, saveNewComment] = useState('');
+  const [savedLocation, saveNewLocation] = useState('');
+  const [savedTemp, saveNewTemp] = useState('');
+  const [savedFeels, saveNewFeels] = useState('');
+  const [savedWind, saveNewWind] = useState('');
+  const [savedDesc, saveNewDesc] = useState('');
   const [data, setData] = useState(null)
   const [location, setLocation] =useState({input: "", isLoading: false})
+
   useEffect(()=>{
     const cb = setTimeout(()=>{
       getLocation(location.input).then(data => {
@@ -19,6 +28,35 @@ const WeatherLines = props =>{
       clearTimeout(cb)
     }
   },[location.input])
+let getSaved = () =>{
+  if (data!=null&&!location.isLoading&& data.weather!=null) {
+    saveNewLocation(data.name)
+    saveNewTemp(data.main.temp)
+    saveNewFeels(data.main.feels_like)
+    saveNewWind(data.wind.speed)
+    saveNewDesc(data.weather[0].main)
+    console.log(savedLocation+" "+savedTemp+" "+savedFeels+" "+savedWind+" "+savedDesc)
+
+  } else {
+    null
+  }
+}
+  const inputHandlerComment = (inputFromUser) => {
+    saveNewComment(inputFromUser);
+  }
+  async function makeComment(){
+  await addDoc(collection(db, "Messages"), {
+    location: savedLocation,
+    comment: savedComment,
+    temperature: savedTemp,
+    feelsLike: savedFeels,
+    wind: savedWind,
+    description: savedDesc
+  });
+  props.closeWData()
+}
+  
+
     return( 
       <Modal visible={props.wDataVis} animationType='slide'  transparent={true}>
         <TopNav></TopNav>
@@ -28,7 +66,7 @@ const WeatherLines = props =>{
       <Text>Loading...</Text>
       </View>}
     {!location.isLoading && data !== null && data.name && (
-    <View style={styles.dataCont}>
+    <View style={styles.dataCont} onLayout={getSaved}>
       <Text>{data.name}</Text>
       {data.weather.length > 0 && <Text>{data.weather[0].main}</Text>}
       <Text>{data.main.temp}</Text>
@@ -43,9 +81,11 @@ const WeatherLines = props =>{
             <TextInput style={styles.weathTextField}
     placeholder='Your location' onChange={(e)=>setLocation({input: e.target.value, isLoading: true})} value={location.input}/>
                 <TextInput style={styles.weathTextField}
-    placeholder='Type your review'/>
+    placeholder='Type your review'
+    value={savedComment}
+    onChangeText={inputHandlerComment}/>
     <View style={styles.buttonContainer}>
-        <Button onPress={props.closeWData} title="Post comment" color="#c4c4c4"></Button>
+        <Button onPress={makeComment} title="Post comment" color="#c4c4c4"></Button>
     </View>
     </View>
     <BotNav></BotNav>
@@ -57,7 +97,7 @@ const styles = StyleSheet.create({
     weatherCont:{
         flex: 0.8,
         flexDirection: 'column',
-        width: 400,
+        width: 450,
         padding:10,
         alignContent: 'center',
         justifyContent: 'space-around',
@@ -69,6 +109,11 @@ const styles = StyleSheet.create({
       },
       weathTextField:{
           borderWidth: 1,
+          padding: 10,
+
+      },
+      textLine:{
+        alignSelf: 'center',
           padding: 10
       },
       buttonContainer:{
